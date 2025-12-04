2822c734e74a8f54d38961e219f898f971fd3aea
Daniel Valença <dvalenca@gmail.com>
2025-12-03 23:51:46 -0300

    apagando sistema anterior da bicicleta

--- firmware/bike/src/diagnostics.h
+++
@@ -1,9 +0,0 @@
-#ifndef DIAGNOSTICS_H
-#define DIAGNOSTICS_H
-
-#include "wifi_scanner.h"
-
-void runDiagnostics();
-void testDataStorage();
-
-#endif
--- firmware/bike/src/serial_menu.h
+++
@@ -1,7 +0,0 @@
-#ifndef SERIAL_MENU_H
-#define SERIAL_MENU_H
-
-void showMenu();
-void handleSerialMenu();
-
-#endif
